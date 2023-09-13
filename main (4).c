eb0a8442b50b925cd7306d2383f59ecf82d44fea
yashpal parmar <144250326+yashpal8078@users.noreply.github.com>
2023-09-13 20:34:26 +0530

    check eligiblity for voting

---
+++ main (4).c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int i;
+    printf("enter your age");
+    scanf("%d",&i);
+    if(18<=i){
+        printf("you are eligible for vote");
+        
+    }
+    else{
+        printf("you are not eligible for vote");
+        
+    }
+    
+    
+    
+    return 0;
+    
+}
